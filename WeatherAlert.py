af3cbdf05338a1192a3a440f007cbffe3275ec11
Ian Gagnon <ian.f.gagnon@gmail.com>
2015-11-29 20:05:13 -0500

    added ability to read forecast and nearly ready to run on server

--- WeatherAlert.py
+++ WeatherAlert.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Nov 08 18:23:44 2015
+Created on Wed Nov 25 19:52:19 2015
 
-@author: Ian Gagnon
+@author: Localadmin
 """
 
 from matplotlib import numpy as np
@@ -11,29 +11,35 @@ from email.MIMEText import MIMEText
 import pywapi
 import string
 import smtplib
+import time
 
 #You must change PrivateData.txt to a txt file containing your private data.
 #An example PrivateData.txt file is given in the repository and titled "PrivateDataEx.txt"
 fromaddr,toaddr,pswd = np.genfromtxt('PrivateData.txt',
                                   dtype= 'str',
                                   delimiter = ',')
-                                  
-noaa_result = pywapi.get_weather_from_noaa('KPSM')
-temp = noaa_result['temp_c']
-wind_speed = noaa_result['wind_mph']
-if wind_speed > 5:
-    
-    msg = MIMEMultipart()
-    msg['From'] = fromaddr
-    msg['To'] = toaddr
-    msg['Subject'] = "Latest Weather"
-    
-    body = "NOAA Portsmouth, NH: Temp={}C\n Wind{}mph\n".format(temp, wind_speed)
-    msg.attach(MIMEText(body, 'plain'))
-    
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(fromaddr, pswd)
-    text = msg.as_string()
-    server.sendmail(fromaddr, toaddr, text)
-    server.quit()
+wind_threshold = 20
+run = 1
+wind = []
+#if run == 1:#change to while to make an infinite loop for server
+weather_com_result = pywapi.get_weather_from_weather_com('USNH0191') 
+for i in range(len(weather_com_result['forecasts'])):
+    wind.append(weather_com_result['forecasts'][i]['day']['wind']['speed'])
+for i in range(len(wind)):
+    if wind[i] > wind_threshold:   
+        msg = MIMEMultipart()
+        msg['From'] = fromaddr
+        msg['To'] = toaddr
+        msg['Subject'] = "Latest Weather"
+        body = "Weather.com Portsmouth, NH:\n Windspeed:{}km/h\n Days out:{}".format(str(wind[i]), str(i+1))
+        print wind[i]
+        print wind_threshold        
+        print body
+        msg.attach(MIMEText(body, 'plain'))
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(fromaddr, pswd)
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddr, text)
+        server.quit()
+        time.sleep(0)
