d5448e0aa1083f73afa47ce3549de6ab46ec6f38
Ian Gagnon <ian.f.gagnon@gmail.com>
2015-11-29 21:32:58 -0500

    basic functioning version works on server

--- WeatherAlert.py
+++ WeatherAlert.py
@@ -9,7 +9,6 @@ from matplotlib import numpy as np
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 import pywapi
-import string
 import smtplib
 import time
 
@@ -20,26 +19,27 @@ fromaddr,toaddr,pswd = np.genfromtxt('PrivateData.txt',
                                   delimiter = ',')
 wind_threshold = 20
 run = 1
-wind = []
-#if run == 1:#change to while to make an infinite loop for server
-weather_com_result = pywapi.get_weather_from_weather_com('USNH0191') 
-for i in range(len(weather_com_result['forecasts'])):
-    wind.append(weather_com_result['forecasts'][i]['day']['wind']['speed'])
-for i in range(len(wind)):
-    if wind[i] > wind_threshold:   
-        msg = MIMEMultipart()
-        msg['From'] = fromaddr
-        msg['To'] = toaddr
-        msg['Subject'] = "Latest Weather"
-        body = "Weather.com Portsmouth, NH:\n Windspeed:{}km/h\n Days out:{}".format(str(wind[i]), str(i+1))
-        print wind[i]
-        print wind_threshold        
-        print body
-        msg.attach(MIMEText(body, 'plain'))
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login(fromaddr, pswd)
-        text = msg.as_string()
-        server.sendmail(fromaddr, toaddr, text)
-        server.quit()
-        time.sleep(0)
+
+while run == 1:#change to while to make an infinite loop for server
+    wind = []    
+    weather_com_result = pywapi.get_weather_from_weather_com('USNH0191') 
+    for i in range(len(weather_com_result['forecasts'])):
+        if weather_com_result['forecasts'][i]['day']['wind']['speed'] == 'calm':
+            weather_com_result['forecasts'][i]['day']['wind']['speed'] = 0
+        wind.append(int(weather_com_result['forecasts'][i]['day']['wind']['speed']))
+    for i in range(len(wind)):
+        if wind[i] > wind_threshold:   
+            msg = MIMEMultipart()
+            msg['From'] = fromaddr
+            msg['To'] = toaddr
+            msg['Subject'] = "Latest Weather"
+            body = "Weather.com Portsmouth, NH:\n Windspeed:{}km/h\n Days out:{}".format(str(wind[i]), str(i+1))
+            msg.attach(MIMEText(body, 'plain'))
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login(fromaddr, pswd)
+            text = msg.as_string()
+            server.sendmail(fromaddr, toaddr, text)
+            server.quit()
+    time.sleep(3600)
+    del wind
